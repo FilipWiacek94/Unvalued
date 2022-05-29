17b55a7b4292251e82cb7c34cbb1095ad628171a
Filip Wiacek <filip.wiacek@tickstar.com>
2022-05-29 13:34:37 +0200

    Initial commit

---
+++ src/main/java/com/unvalued/Unvalued/UnvaluedApplication.java
@@ -0,0 +1,13 @@
+package com.unvalued.Unvalued;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnvaluedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnvaluedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unvalued/Unvalued/UnvaluedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unvalued.Unvalued;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnvaluedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
